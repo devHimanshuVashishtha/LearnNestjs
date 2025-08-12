70978c47f5978b021a423c0a581e582d62414ca0
devHimanshuVashishtha <himanshuvashishtha001.hp@gmail.com>
2025-08-12 17:01:01 +0530

    added the mongodb and perform CURD Operation

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,15 +1,57 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { UserModule } from './user/user.module';
+import { Logger, Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
-
+import { ConfigModule } from '@nestjs/config';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [UserModule,
-    MongooseModule.forRoot("mongodb://localhost/nestjs-user")
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+
+    MongooseModule.forRootAsync({
+      useFactory: async () => {
+        let uri = process.env.MONGO_URI;
+
+        if (!uri) {
+          uri = 'mongodb://localhost/nestjs-user'
+        }
+
+        Logger.log(`connecting MongoDb`);
+
+        return {
+          uri,
+          connectionFactory: (connection) => {
+            Logger.log('MongoDB connection started');
+            connection.on('connected', () => {
+              Logger.log('MongoDB connected');
+            });
+            connection.on('error', (err) => {
+              Logger.error('MongoDB connection error', err);
+            });
+            connection.on('disconnected', () => {
+              Logger.warn('MongoDB disconnected');
+            });
+            setImmediate(() => {
+              switch (connection.readyState) {
+                case 1:
+                  Logger.log('MongoDB connected');
+                  break;
+                case 2:
+                  Logger.log('MongoDB connecting');
+                  break;
+                case 0:
+                  Logger.warn('MongoDB not connected');
+                  break;
+                default:
+                  Logger.log(`MongoDB state: ${connection.readyState}`, 'Mongoose');
+              }
+            });
+            return connection;
+          }
+        };
+      },
+    }),
+
+    UserModule,
   ],
-  controllers: [AppController],
-  providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getUsers(): string[] {
-    return ["user1", 'user2'];
+    return ["1", "2"];
   }
 }
