d4970033158dc843c8a6721cefbc72d272fefb2b
devHimanshuVashishtha <himanshuvashishtha001.hp@gmail.com>
2025-08-12 12:47:50 +0530

    update user and delete using id and param

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,11 +1,11 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Put, Param, Delete, ParseIntPipe } from '@nestjs/common';
 import { UserService } from './user.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 
 @Controller('user')
 export class UserController {
-  constructor(private readonly userService: UserService) {}
+  constructor(private readonly userService: UserService) { }
 
   @Post()
   create(@Body() createUserDto: CreateUserDto) {
@@ -15,5 +15,18 @@ export class UserController {
   @Get()
   findAll() {
     return this.userService.findAll();
+  }
+  @Put(':id')
+  update(
+    @Param('id', ParseIntPipe) id: number,
+    @Body() UpdateUserDto: Partial<CreateUserDto>
+  ) {
+    return this.userService.update(id, UpdateUserDto)
+  }
+  @Delete(':id')
+  remove(
+    @Param('id', ParseIntPipe) id: number) {
+    this.userService.delete(id);
+    return { message: `user :${id} is deleted` }
   }
 }
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,18 +1,33 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 type User = CreateUserDto & { id: number };
 
 @Injectable()
 export class UserService {
-  private users :User []= [];
+  private users: User[] = [];
   create(createUserDto: CreateUserDto) {
     const user = { id: Date.now(), ...createUserDto }
     this.users.push(user)
     return user
   }
 
-  findAll() {
+  findAll(): User[] {
     return this.users;
+  }
+  update(id: number, UpdateUserDto: Partial<CreateUserDto>): User {
+    const index = this.users.findIndex(user => user.id === id)
+    if (index === -1) {
+      throw new NotFoundException(`User id not found id:${id}`)
+    }
+    this.users[index] = { ...this.users[index], ...UpdateUserDto }
+    return this.users[index]
+  }
+  delete(id: number): void {
+    const index = this.users.findIndex(user => user.id === id)
+    if (index === -1) {
+      throw new NotFoundException(`User not found for id:${id} or provide me correct id`)
+    }
+    this.users.splice(index, 1)
   }
 }
