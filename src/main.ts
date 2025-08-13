f8f1a8ad1e1d798571c2edd17a810a17a57c5088
devHimanshuVashishtha <himanshuvashishtha001.hp@gmail.com>
2025-08-13 17:03:51 +0530

    .

--- src/main.ts
+++ src/main.ts
@@ -10,6 +10,6 @@ async function bootstrap() {
   app.useStaticAssets(join(__dirname, '..', 'uploads'), {
     prefix: '/uploads/',
   });
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
